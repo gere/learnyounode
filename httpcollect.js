8789f2ee04f5dd9725e0b5a9998bffc6f03764a0
gere <gerevini@gmail.com>
2015-10-31 14:05:08 +0100

    http collect completed

--- httpcollect.js
+++ httpcollect.js
@@ -1,1 +1,15 @@
 var http = require('http');
+http.get(process.argv[2], function(response){
+	var result = "";
+	response
+	.setEncoding("utf8")
+	.on("data", function(data) {
+		result += data;
+	})	
+	.on("end", function(data) {
+		console.log(result.length + "\n" + result);
+	});
+})
+.on("error", function(e) {
+	return console.error(e);
+});
